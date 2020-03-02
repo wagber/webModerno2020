a52285de410f4c0c8d6ce3d55f29a4940c3cc3de
Wagber <wagber.dev@gmail.com>
2020-03-02 18:38:23 -0300

    Weak Typing

---
+++ javaScript/fundamentals/weakTyping.js
@@ -0,0 +1,14 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+//JavaScript tem tipagem fraca
+
+qualquer = 3.1516 //observe que não foi colocado novamente o let, pois ao colocar dará erro
+console.log(qualquer)
+console.log(typeof qualquer)
+//Em javaScript uma variável pode ter multiplos valores
+
+//Evite nomes genérios e siglas
+let valor = ''
+let numero = 1
+let pqp = false //pqp? => Produto Químico Perigoso
