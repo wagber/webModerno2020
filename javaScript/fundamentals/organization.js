1f9e85dc567e6ba6484e94f5d1f91b80dc6bd7b2
Wagber <wagber.dev@gmail.com>
2020-03-02 16:30:31 -0300

    Organization

---
+++ javaScript/fundamentals/organization.js
@@ -0,0 +1,18 @@
+console.log("Sentença de código") //Sentença de código
+
+{   //Bloco de código
+    //Posso ter bloco de código associado a função, classe,
+    //objeto, repetição, estrutura de controle, teste,
+    //A função de um bloco é de agrupar sentenças de códigos
+    //Posso ter um bloco dentro de outro
+    //Posso ter um conjunto de sentenças
+    console.log('olá ')
+    console.log('Mundo!')
+}
+
+/*
+Como JavaScripit é organizado?
+Em sentenças de código que podem ou não ser terminados com
+ponto e vírgula.
+E em blocos de códigos delimitados por chave.
+*/
