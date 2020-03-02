c0834ad0f1da3010e9ce179107ca3f0cb2d40d91
Wagber <wagber.dev@gmail.com>
2020-03-02 16:28:41 -0300

    Comment

---
+++ javaScript/fundamentals/comment.js
@@ -0,0 +1,13 @@
+// comentário de uma única linha
+console.log('linha 1')
+
+/*
+comentário de múltiplas linhas
+*/
+console.log('linha 2')
+
+/* comentário de múltiplas linhas
+* Esta forma é apenas para ficar mais detalhado
+*
+*/
+console.log('linha 3')
