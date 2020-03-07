a50d8f5f3a94d5429d11439087260b6a64d2f901
Wagber <wagber.dev@gmail.com>
2020-03-06 21:33:23 -0300

    Assignment

---
+++ javaScript/fundamentals/assignment.js
@@ -0,0 +1,17 @@
+const a = 7 //o Simbolo de = não está para comparação, más para atribuição
+let b = 3
+
+b += a //atribuição aditiva -> atribuição juntamente com adição -> é o mesmo que b = b + a
+console.log(b)
+
+b -= 4 //b = b -4
+console.log(b)
+
+b *= 2 //b = b * 2
+console.log(b)
+
+b /= 2 //b = b / 2
+console.log(b)
+
+b %= 2 //b = b % 2 -> operação modular
+console.log(b)
